d9e3e94e6803a811ca547233c303e822a784590c
Fabio Alessandro Locati <fabiolocati@gmail.com>
2013-05-22 14:29:46 +0200

    Add routing to the functions

--- app/routes.php
+++ app/routes.php
@@ -1,17 +1,15 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
+Route::get('help', array(function () { return View::make('help'); }));
 
-Route::get('/', function()
-{
-	return View::make('hello');
-});
+Route::get('csv', array('uses' => 'CsvController@all'));
+Route::get('csv/tra/{from}/{to}', array('uses' => 'CsvController@all'));
+Route::get('csv/dal/{from}', array('uses' => 'CsvController@all'));
+Route::get('csv/al/{to}', array('uses' => 'CsvController@to'));
+Route::get('csv/number/{n}', array('uses' => 'CsvController@number'));
+
+Route::get('/', array(function () { return View::make('index'); }));
+Route::get('dal/{d}', array(function ($d) { return View::make('index')->with('d', $d); }));
+Route::get('al/{a}', array(function ($a) { return View::make('index')->with('a', $a); }));
+Route::get('tra/{d}/{a}', array(function ($d, $a) { return View::make('index')->with('d', $d)->with('a', $a); }));
+Route::get('number/{n}', array(function ($n) { return View::make('index')->with('n', $n); }));
